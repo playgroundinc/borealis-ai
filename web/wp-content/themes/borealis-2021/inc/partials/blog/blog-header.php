18f8c8e7cf27eb7c7610fa45908779dbb21547f3
Ky Capstick <ky.capstick@gmail.com>
2022-01-25 11:33:33 -0500

    99 add in featured image and mockup where remaining work is needed

--- web/wp-content/themes/borealis-2021/inc/partials/blog/blog-header.php
+++ web/wp-content/themes/borealis-2021/inc/partials/blog/blog-header.php
@@ -11,21 +11,23 @@ if (!function_exists('pg_generate_blog_header')) {
         ob_start();
         ?>
         <div class="pt-41">
-            <div class="w-8/12">
+            <div class="md:w-8/12">
                 <div class="flex flex-col-reverse">
                     <h1 class="h1 pt-8"><?php echo esc_html(the_title()) ?></h1>
-                    <div class="flex">
+                    <div class="md:flex">
                         <p class="paragraph-small text-shade-grey-700 pr-4"><?php echo !empty($type[0]) ? esc_html($type[0]->name) : 'Blog' ?></p>
                         <?php if (!empty($research_areas)): ?>
-                            <p class="paragraph-small text-shade-grey-700 pl-4 border-l border-shade-black-400"><?php echo esc_html($research_areas)?></p>
+                            <p class="paragraph-small text-shade-grey-700 md:pl-4 md:border-l border-shade-black-400"><?php echo esc_html($research_areas)?></p>
                         <?php endif;?>
                     </div>
                 </div>
                 <div class="flex pt-8">
                     <p class="h4"><?php echo esc_attr($publication_date); ?></p>
-                </div>
-               
+                </div> 
+                <!-- TODO: Add Read Time -->
+                <!-- TODO: Add Authors -->
             </div>
+            <!-- TODO: Add series -->
         </div>
         <?php
         return ob_get_clean();
--- web/wp-content/themes/borealis-2021/single-research-blogs.php
+++ web/wp-content/themes/borealis-2021/single-research-blogs.php
@@ -8,22 +8,35 @@
  */
 
 get_header();
+$hero_image_url = get_the_post_thumbnail_url($post->ID, 'full')
 ?>
-    <div class="research-blog-sidebar">
-       <?php echo pg_generate_blog_sidebar($post->ID); ?>
-    </div>
-    <div class="page__content">
-        <?php 
-        if ( have_posts() ) : 
-            while ( have_posts() ) : 
-                the_post();
-            ?>
-            <?php
-                the_content();
-            endwhile;
-        endif;
-        ?>
-    </div>
+    <?php if (isset($hero_image_url) && !empty($hero_image_url)): ?>
+        <div aria-hidden="true" class="pt-100 mt-19 bg-cover bg-no-repeat bg-center" style="background-image: url(<?php echo esc_url_raw($hero_image_url) ?>)"></div>
+    <?php endif; ?>
+    <main class="main-content container">
+        <div class="pt-14 flex">
+            <aside class="w-1/6">
+                <div class="research-blog-sidebar">
+                    <?php echo pg_generate_blog_sidebar($post->ID); ?>
+                </div>
+            </aside>
+            <div class="page__content w-5/6">
+                <div class="mx-auto w-5/6">
+                    <?php 
+                    if ( have_posts() ) : 
+                        while ( have_posts() ) : 
+                            the_post();
+                        ?>
+                        <?php
+                            the_content();
+                        endwhile;
+                    endif;
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
+
 <?php
 get_footer();
 
