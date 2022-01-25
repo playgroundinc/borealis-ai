8f5a1e4d1bad0a850f86f4ab7998a7ad3da802a5
Braeden Craig <braedencraig@gmail.com>
2022-01-25 10:39:32 -0500

    BA-90 comment pr

--- web/wp-content/themes/borealis-2021/dist/build.js
+++ web/wp-content/themes/borealis-2021/dist/build.js
@@ -10471,49 +10471,64 @@ function search() {
   }
 
   const checkboxSearchForm = container => {
-    let selections = {};
-    const checkboxEls = document.querySelectorAll("input[type='checkbox']");
+    let selections = {}; // Checkbox elements
+
+    const checkboxEls = document.querySelectorAll("input[type='checkbox']"); // Params and current values
+
     const params = new _classes_class_query_params__WEBPACK_IMPORTED_MODULE_1__["default"](container.id);
-    const currentValues = params.getParam(container.id);
-    const clearAll = document.querySelector(".clear-checkboxes");
+    const currentValues = params.getParam(container.id); // Clear all button
+
+    const clearAll = document.querySelector(".clear-checkboxes"); // Topics increment/decrement vars
+
     const topics = document.querySelector(".topics");
-    let topicsNum = parseInt(topics.innerHTML);
-    const taxonomyButton = document.getElementById(container.id);
+    let topicsNum = parseInt(topics.innerHTML); // Taxonomy button
+
+    const taxonomyButton = document.getElementById(container.id); // Adds taxonomy to query params if not already present
+
     taxonomyButton.addEventListener("click", addTaxonomyParam);
 
     function addTaxonomyParam() {
       if (window.location.href.indexOf(container.id) > -1 === false) {
-        params.appendParam(container.id, '');
+        params.appendParam(container.id, "");
       }
-    }
+    } // Handles clear all button functionality
+
 
     clearAll.addEventListener("click", clearAllCheckboxes);
+
+    function clearAllCheckboxes() {
+      // Uncheck all checkboxes
+      checkboxEls.forEach(checkbox => {
+        checkbox.checked = false;
+      }); // Clear all params
+
+      window.location.href = window.location.origin + window.location.pathname; // Set selections to empty object
+
+      selections = {}; // Update topics number
+
+      topicsNum = 0;
+      topics.innerHTML = topicsNum;
+    } // Handles checking of checkboxes and updating of params
+
 
     if (currentValues) {
       checkboxEls.forEach(checkbox => {
         if (currentValues.split(",").includes(checkbox.value)) {
-          topicsNum += 1;
-          checkbox.checked = true;
+          // Increment topics number
+          topicsNum += 1; // Set checkbox to checked
+
+          checkbox.checked = true; // Add to selections object
+
           selections[checkbox.value] = {
             name: checkbox.value,
             value: checkbox.value
           };
         }
       });
-    }
+    } // Set topics number
 
-    topics.innerHTML = topicsNum;
 
-    function clearAllCheckboxes() {
-      checkboxEls.forEach(checkbox => {
-        checkbox.checked = false;
-      });
-      params.setParam("", "");
-      window.location.href = window.location.origin + window.location.pathname;
-      selections = {};
-      topicsNum = 0;
-      topics.innerHTML = topicsNum;
-    }
+    topics.innerHTML = topicsNum; // Add event listener to checkboxes to updateUrl with term id's
 
     for (let i = 0; i < checkboxEls.length; i++) {
       checkboxEls[i].addEventListener("click", updateUrl);
@@ -10521,15 +10536,19 @@ function search() {
 
     function updateUrl(e) {
       if (e.target.checked) {
+        // Increment topics number
         topicsNum += 1;
-        topics.innerHTML = topicsNum;
+        topics.innerHTML = topicsNum; // Add to selections object
+
         selections[e.target.id] = {
           name: e.target.name,
           value: e.target.value
         };
       } else {
+        // Decrement topics number
         topicsNum -= 1;
-        topics.innerHTML = topicsNum;
+        topics.innerHTML = topicsNum; // Remove from selections object AND params
+
         params.setParam("");
         delete selections[e.target.id];
       }
@@ -10538,15 +10557,18 @@ function search() {
 
       for (let key in selections) {
         results.push(selections[key].value);
-      }
+      } // Update params
+
 
       params.setParam(results.join(","));
     }
-  };
+  }; //   Add checkbox functionality to all taxonomy search forms
+
 
   const checkboxContainers = document.querySelectorAll(".checkbox-form");
   checkboxContainers.forEach(checkboxContainer => checkboxSearchForm(checkboxContainer));
 }
+;
 
 /***/ }),
 
--- web/wp-content/themes/borealis-2021/src/js/scripts/search.js
+++ web/wp-content/themes/borealis-2021/src/js/scripts/search.js
@@ -13,66 +13,83 @@ export default function search() {
 
   const checkboxSearchForm = (container) => {
     let selections = {};
+    // Checkbox elements
     const checkboxEls = document.querySelectorAll("input[type='checkbox']");
+    // Params and current values
     const params = new QueryParams(container.id);
     const currentValues = params.getParam(container.id);
+    // Clear all button
     const clearAll = document.querySelector(".clear-checkboxes");
+    // Topics increment/decrement vars
     const topics = document.querySelector(".topics");
     let topicsNum = parseInt(topics.innerHTML);
-
+    // Taxonomy button
     const taxonomyButton = document.getElementById(container.id);
+
+    // Adds taxonomy to query params if not already present
     taxonomyButton.addEventListener("click", addTaxonomyParam);
-
     function addTaxonomyParam() {
-        if (window.location.href.indexOf(container.id) > -1 === false) {
-            params.appendParam(container.id, '');
-        } 
+      if (window.location.href.indexOf(container.id) > -1 === false) {
+        params.appendParam(container.id, "");
+      }
     }
 
+    // Handles clear all button functionality
     clearAll.addEventListener("click", clearAllCheckboxes);
-
-    if (currentValues) {
-      checkboxEls.forEach((checkbox) => {
-        if (currentValues.split(",").includes(checkbox.value)) {
-          topicsNum += 1;
-          checkbox.checked = true;
-          selections[checkbox.value] = {
-            name: checkbox.value,
-            value: checkbox.value,
-          }
-        }
-      });
-    }
-    topics.innerHTML = topicsNum;
-
     function clearAllCheckboxes() {
+      // Uncheck all checkboxes
       checkboxEls.forEach((checkbox) => {
         checkbox.checked = false;
       });
-      params.setParam("", "");
+      // Clear all params
       window.location.href = window.location.origin + window.location.pathname;
+      // Set selections to empty object
       selections = {};
+      // Update topics number
       topicsNum = 0;
       topics.innerHTML = topicsNum;
     }
 
+    // Handles checking of checkboxes and updating of params
+    if (currentValues) {
+      checkboxEls.forEach((checkbox) => {
+        if (currentValues.split(",").includes(checkbox.value)) {
+          // Increment topics number
+          topicsNum += 1;
+          // Set checkbox to checked
+          checkbox.checked = true;
+          // Add to selections object
+          selections[checkbox.value] = {
+            name: checkbox.value,
+            value: checkbox.value,
+          };
+        }
+      });
+    }
+    // Set topics number
+    topics.innerHTML = topicsNum;
+
+    // Add event listener to checkboxes to updateUrl with term id's
     for (let i = 0; i < checkboxEls.length; i++) {
       checkboxEls[i].addEventListener("click", updateUrl);
     }
-
     function updateUrl(e) {
       if (e.target.checked) {
+        // Increment topics number
         topicsNum += 1;
         topics.innerHTML = topicsNum;
+        // Add to selections object
         selections[e.target.id] = {
           name: e.target.name,
           value: e.target.value,
         };
       } else {
+        // Decrement topics number
         topicsNum -= 1;
         topics.innerHTML = topicsNum;
+        // Remove from selections object AND params
         params.setParam("");
-        delete selections[e.target.id];    
+        delete selections[e.target.id];
       }
 
       const results = [];
@@ -80,13 +97,14 @@ export default function search() {
       for (let key in selections) {
         results.push(selections[key].value);
       }
-
+      // Update params
       params.setParam(results.join(","));
     }
   };
 
+  //   Add checkbox functionality to all taxonomy search forms
   const checkboxContainers = document.querySelectorAll(".checkbox-form");
   checkboxContainers.forEach((checkboxContainer) =>
     checkboxSearchForm(checkboxContainer)
   );
-}
+};
