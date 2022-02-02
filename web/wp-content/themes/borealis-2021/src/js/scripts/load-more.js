44b5d831b961ad2b53fe57f34704a398b544e6bd
Ky Capstick <ky.capstick@gmail.com>
2022-02-02 12:17:08 -0500

    FIX - Add in catch for loader

--- web/wp-content/themes/borealis-2021/src/js/scripts/load-more.js
+++ web/wp-content/themes/borealis-2021/src/js/scripts/load-more.js
@@ -2,6 +2,8 @@ import Loader from "./classes/class-loader";
 export default function loadMore() {
     const loadMoreButton = document.querySelector('.load-more');
     const list = document.querySelector('.posts-listing');
-    const PostLoader = new Loader(loadMoreButton, list, ['research-areas']);
-    PostLoader.init();
+    if (loadMoreButton && list) {
+        const PostLoader = new Loader(loadMoreButton, list, ['research-areas']);
+        PostLoader.init();
+    }
 }
