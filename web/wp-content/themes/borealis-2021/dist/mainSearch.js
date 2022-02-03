6271b8133d0b2d241477cd06c1dc97cff7ad874c
Braeden Craig <braedencraig@gmail.com>
2022-02-03 11:03:17 -0500

    ba-117 search page add checks in js

--- web/wp-content/themes/borealis-2021/dist/mainSearch.js
+++ web/wp-content/themes/borealis-2021/dist/mainSearch.js
@@ -7,14 +7,14 @@ function mainSearch() {
   const searchInput = document.getElementById('search');
   const searchIconOpen = document.querySelector('#open-search');
   const searchIconClose = document.querySelector('#close-search');
-  searchIconClose.addEventListener('click', () => {
+  searchIconClose && searchIconClose.addEventListener('click', () => {
     if (!searchIconClose.classList.contains('hidden')) {
       searchInput.value = '';
       searchIconClose.classList.add('hidden');
       searchIconOpen.classList.remove('hidden');
     }
   });
-  searchInput.addEventListener('input', e => {
+  searchInput && searchInput.addEventListener('input', e => {
     if (e.target.value) {
       searchInput.classList.remove('text-shade-grey-700');
       searchInput.classList.add('text-primary-navy-400');
--- web/wp-content/themes/borealis-2021/src/js/scripts/main-search.js
+++ web/wp-content/themes/borealis-2021/src/js/scripts/main-search.js
@@ -3,7 +3,7 @@ function mainSearch() {
     const searchIconOpen =  document.querySelector('#open-search');
     const searchIconClose = document.querySelector('#close-search');
 
-    searchIconClose.addEventListener('click', () => {
+    searchIconClose && searchIconClose.addEventListener('click', () => {
         if(!searchIconClose.classList.contains('hidden')) {
             searchInput.value = '';
             searchIconClose.classList.add('hidden');
@@ -11,7 +11,7 @@ function mainSearch() {
         }
     })
 
-    searchInput.addEventListener('input', e => {
+    searchInput && searchInput.addEventListener('input', e => {
         if(e.target.value) {
             searchInput.classList.remove('text-shade-grey-700');
             searchInput.classList.add('text-primary-navy-400');
