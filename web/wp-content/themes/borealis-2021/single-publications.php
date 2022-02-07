b202ab6ddbda7de3f2389002253e137720625dc1
Braeden Craig <braedencraig@gmail.com>
2022-02-07 15:09:04 -0500

    ba-118 search results remove dist

---
+++ web/wp-content/themes/borealis-2021/single-publications.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package pg-wp-starter
+ */
+
+get_header();
+$hero_image_url = get_the_post_thumbnail_url($post->ID, 'full');
+$title = get_the_title($post->ID);
+$publication_date = get_post_meta($post->ID, 'publication_date', true);
+$authors = get_post_meta($post->ID, 'authors', true);
+$url = get_permalink($post->ID);
+?>
+<?php if (isset($hero_image_url) && !empty($hero_image_url)) : ?>
+    <div aria-hidden="true" class="pt-100 mt-19 bg-cover bg-no-repeat bg-center" style="background-image: url(<?php echo esc_url_raw($hero_image_url) ?>)"></div>
+<?php endif; ?>
+<main class="main-content container research-blog">
+   asdasds
+</main>
+
+<?php
+get_footer();
