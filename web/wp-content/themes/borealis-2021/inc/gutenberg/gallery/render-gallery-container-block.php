5e75d74f34ea1d2a10facbebcaeca07756d089ce
Ky Capstick <ky.capstick@gmail.com>
2022-02-22 12:41:37 -0500

    89 Build out drag functionality

--- web/wp-content/themes/borealis-2021/inc/gutenberg/gallery/render-gallery-container-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/gallery/render-gallery-container-block.php
@@ -18,6 +18,7 @@ if ( function_exists( 'register_block_type' ) ) {
         $namespace . '/gallery-container',
         array(
             'render_callback' => 'pg_render_gallery_container_block',
+            'script' => 'custom-gallery'
         )
     );
 }
@@ -49,21 +50,25 @@ if ( ! function_exists( 'pg_render_gallery_container_block' ) ) {
             <div class="md:flex container">
                 <div class="md:w-4/12">
                     <?php if (!empty($attributes->title)): ?>                   
-                        <h2 class="h2 pr-8"><?php echo esc_html($attributes->title) ?></h2>
+                        <h2 class="h3 md:h2 pr-8"><?php echo esc_html($attributes->title) ?></h2>
                     <?php endif; ?>
                 </div>
                 <div class="md:w-8/12">
                     <?php if (strlen($attributes->description) > 0): ?>
-                        <p class="paragraph-lg"><?php echo esc_html($attributes->description); ?></p>
+                        <p class="paragraph md:paragraph-lg pt-8 md:pt-0"><?php echo esc_html($attributes->description); ?></p>
                     <?php endif; ?>
                     <?php if (!empty($attributes->link) && strlen($attributes->link_text) > 0): ?>
-                        <a class="py-4 text-link paragraph" href="<?php echo esc_attr($attributes->link)?>"><?php echo esc_html($attributes->link_text); ?></a>
+                        <a class="py-4 h4 text-shade-black-400 visited:text-primary-electric-purple-400 paragraph flex items-center" href="<?php echo esc_attr($attributes->link)?>">
+                            <?php echo esc_html($attributes->link_text); ?>
+                            <span aria-hidden="true" class="rotate-90 pb-6"><?php echo pg_render_icon('arrow-white') ?></span>
+                        </a>
+
                     <?php endif; ?>
                 </div>                
             </div>
             <div>
-                <div class="mx-auto relative pt-gallery">
-                    <ul class="flex absolute left-0 top-0 bottom-0 max-w-full overflow-scroll pl-gallery-offset pr-25">
+                <div class="mx-auto relative pt-gallery transition-all duration-400">
+                    <ul class="flex absolute left-0 top-0 bottom-0 max-w-full overflow-scroll pl-16 lg:pl-gallery-offset items-center pr-25 lg:pr-gallery-offset custom-gallery">
                         <?php foreach ( $block['innerBlocks'] as $index => $inner_block ) : ?>
                             <?php if ($inner_block['blockName'] === $namespace . '/custom-image'): ?>
                                     <?php 
@@ -90,14 +95,15 @@ if ( ! function_exists( 'pg_render_gallery_container_block' ) ) {
                                         continue;
                                     }
                                     $post = get_post($attributes->post);
+                                    $slug = pg_slugify(sanitize_text_field($post->post_title));
                                     $image = get_the_post_thumbnail_url($post->ID, 'team-member');
                                     $position = get_post_meta($post->ID, 'position', true);
                                     $url = get_permalink($post->ID);
                                 ?>
                                 <li class="nth-child-2:ml-5  basis-team-member shrink-0 grow-0">
-                                    <a href="<?php echo esc_url_raw($url)?>" class="block">
-                                        <img class="block rounded-large" src="<?php echo esc_url_raw($image); ?>"/>
-                                        <p class="h4 pt-12"><?php echo esc_html($post->post_title); ?></p>
+                                    <a href="<?php echo esc_url_raw($url)?>" class="block" aria-labelledby="<?php echo esc_attr($slug)?>">
+                                        <img class="block rounded-large" src="<?php echo $image && strlen($image) > 0 ? esc_url_raw($image) : get_bloginfo('stylesheet_directory') . '/src/images/teamMember.png' ?>"/>
+                                        <p id="<?php echo esc_attr($slug) ?>" class="h4 pt-12"><?php echo esc_html($post->post_title); ?></p>
                                         <?php if ($position && strlen($position) >0):?>
                                             <p class="paragraph pt-4"><?php echo esc_html($position) ?></p>
                                         <?php endif; ?>
@@ -109,6 +115,17 @@ if ( ! function_exists( 'pg_render_gallery_container_block' ) ) {
                 </div>
             </div>
         </div>
+        <style>
+            .custom-gallery:hover {
+                cursor: url(<?php echo get_bloginfo('stylesheet_directory') . '/src/images/dragCursor.png' ?>), auto;
+            }
+            .custom-gallery li:hover {
+                cursor: url(<?php echo get_bloginfo('stylesheet_directory') . '/src/images/dragCursor.png' ?>), auto;
+            }
+            .custom-gallery a:hover {
+                cursor: url(<?php echo get_bloginfo('stylesheet_directory') . '/src/images/dragCursor.png' ?>), auto;
+            }
+        </style>
         <?php
         return ob_get_clean();
     }
