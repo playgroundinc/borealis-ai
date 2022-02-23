86356a4c9fce70e55c5dc67c3f3da4ae05eb3904
Ky Capstick <ky.capstick@gmail.com>
2022-02-23 12:34:39 -0500

    82 add initial styles for product list

---
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/products/render-products-item.php
@@ -0,0 +1,39 @@
+<?php 
+
+if (!function_exists('pg_render_products_item')) {
+    function pg_render_products_item($block, $link) {
+        $fields     = array(
+            'post' => 0,
+        );
+
+        $attributes = pg_get_attributes( $block['attrs'], $fields );
+        if (isset($attributes->post) && intval($attributes->post) > 0): // Start of post ID check
+            $post = get_post($attributes->post);
+            $image = get_the_post_thumbnail_url($post->ID, 'product-single');
+            $market = get_post_meta($post->ID, 'market', true);
+            $description = get_post_meta($post->ID, 'description', true);
+        ?> 
+            <li class="block border-b border-color-shade-grey-500">
+                <a class="block py-15 hover:bg-shade-grey-100 bg-shade-white-400 transition-background-color duration-300 group" href="<?php echo esc_attr(get_permalink($post->ID))?>">
+                    <div class="container flex items-center">
+                        <div class="basis-1/3 shrink-0">
+                            <?php if (isset($market) && strlen($market) > 0): ?>
+                                <p class="h3"><?php echo esc_html($market); ?></p>
+                            <?php endif; ?>
+                        </div>
+                        <div class="basis-1/2 shrink-0 relative">  
+                            <p class="paragraph"><?php echo $description && strlen($description) > 0 ? esc_html($description) : esc_html($post->post_title); ?></p>
+                            <div class="opacity-0 invisible absolute rounded-large overflow-hidden right-6 -top-20 group-hover:opacity-100 group-hover:visible transition-all duration-400">
+                                <img src="<?php echo esc_url_raw($image)?>" />
+                            </div>
+                        </div>
+                        <div class="basis-1/6 shrink-0">
+                            <p class="primary-button flex items-center"><?php echo esc_html('View Product') ?><span class="pl-8"><?php echo pg_render_icon('arrow-white'); ?></span></p>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        <?php 
+        endif; // End of post ID check
+    }
+}
---
+++ web/wp-content/themes/borealis-2021/src/js/blocks/products/product-container.jsx
@@ -0,0 +1,67 @@
+import { namespace } from '../helper-functions/constants';
+
+import CustomRichText from '../reusable/custom-richtext-component.jsx';
+
+export default function productContainerBlock() {
+	const { registerBlockType } = wp.blocks;
+	const {
+		InnerBlocks,
+    } = wp.blockEditor;
+    const { i18n } = wp;
+
+	const blockSlug = "product-container";
+	const blockTitle = "Products List Container";
+	const blockDescription = "Creates a container for a products list.";
+	const blockCategory = "common";
+    const blockIcon = "editor-justify"; // Dashicons: https://developer.wordpress.org/resource/dashicons/
+
+	registerBlockType(`${namespace}/${blockSlug}`, {
+		title: i18n.__(blockTitle),
+		description: i18n.__(blockDescription),
+		category: blockCategory,
+        icon: blockIcon,
+        attributes: {
+            title: {
+                type: "String",
+                default: "",
+            }
+        },
+		edit: (props, editor = false, save = false ) => {
+            const { setAttributes, attributes } = props;
+            const { title } = attributes;
+
+			function updateAttributeValue(attribute, value) {
+				setAttributes({ [attribute]: value });
+			}
+
+			return [
+                <div className={`custom-container` }>
+                    <p className="block-title">Products Container</p>
+                    <CustomRichText 
+                        components={[
+                            {
+                                reference: 'title',
+                                value: title,
+                                tagName: 'h2',
+                                classes: ['h2'],
+                                settings: [],
+                                placeholder: 'Provide a title (optional)',
+                            },
+                        ]}
+                        onChange={ ( attribute, change ) => { updateAttributeValue(attribute, change) } }
+                    />
+                    {save ? (
+                        <InnerBlocks.Content />
+                    ) : (
+                        <InnerBlocks
+                            allowedBlocks={[`${namespace}/select-product`]}
+                        />
+                    )}
+                </div>, 
+            ];
+		},
+		save: () => {
+			return <InnerBlocks.Content />;
+		},
+	});
+}
