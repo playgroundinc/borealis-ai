1a4a1119675f678d05aa2b3a98f7f6d6bd867f71
Ky Capstick <ky.capstick@gmail.com>
2022-02-23 14:48:08 -0500

    82 product container and items

--- web/wp-content/themes/borealis-2021/inc/gutenberg/products/render-products-list-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/products/render-products-list-block.php
@@ -42,7 +42,7 @@ if ( ! function_exists( 'pg_render_products_container_block' ) ) {
         ob_start();
         ?>
         <div class="custom-component animated-element">
-            <div class="container pb-15">
+            <div class="container pb-10">
                 <?php if (!empty($attributes->title)): ?>                   
                     <h2 class="h3 text-shade-grey-700"><?php echo esc_html($attributes->title) ?></h2>
                 <?php endif; ?>
