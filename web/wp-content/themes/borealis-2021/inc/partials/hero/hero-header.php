dccd5304785d9352a620034dff9ecaa85b27de50
Braeden Craig <braedencraig@gmail.com>
2022-02-25 12:59:21 -0500

    BA-124 hero animation

---
+++ web/wp-content/themes/borealis-2021/inc/partials/hero/hero-header.php
@@ -0,0 +1,16 @@
+<?php
+if (!function_exists('pg_generate_hero_header')) {
+    function pg_generate_hero_header($id)
+    {
+        $allowed_html = pg_allowed_html();
+        ob_start();
+?>
+        <div>
+            <canvas class="absolute top-0 canvas" id="canvas"></canvas>
+            <div class="w-full md:w-6/12 h2 md:h1 relative z-10 pt-72 text-shade-white-400">Artificial Intelligence for finance.</div>
+            <!-- <div class="w-8/12 h1 relative z-10 pt-72 text-shade-white-400"><?php echo esc_html(the_title()) ?></div> -->
+        </div>
+<?php
+        return ob_get_clean();
+    }
+}
