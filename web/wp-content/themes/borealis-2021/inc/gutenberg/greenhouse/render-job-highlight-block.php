20ec6e2fa84a240e3d35625c830cd6d801871695
Ky Capstick <ky.capstick@gmail.com>
2022-02-25 12:38:53 -0500

    109 hide when not on single page

--- web/wp-content/themes/borealis-2021/inc/gutenberg/greenhouse/render-job-highlight-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/greenhouse/render-job-highlight-block.php
@@ -9,16 +9,16 @@ if (!function_exists('pg_render_job_highlight_block')) {
             'title',
         );
         $attributes = pg_get_attributes($fields, $attrs);
-        $image = wp_get_attachment_image_url($attributes->image_id, 'full');
+        $image = wp_get_attachment_image_url($attributes->image_id, 'job-highlight');
         ob_start();
         ?>
-        <div class="lg:flex justify-between container md:container pt-7">
-            <div class="basis-3/8 shrink-0 text-center">
-                <div class="rounded-large overflow-hidden inline-block">
-                    <img src="<?php echo esc_url_raw($image) ?>" alt="<?php echo esc_attr($attributes->image_alt) ?>">
+        <div class="justify-between container md:container pt-7 <?php echo is_single() ? 'tb:flex' : 'hidden'?>">
+            <div class="basis-3/8 lg:w-3/8 shrink-0 grow-0 flex justify-center items-center">
+                <div class="rounded-large overflow-hidden inline-block object-contain">
+                    <img class="block max-w-full" src="<?php echo esc_url_raw($image) ?>" alt="<?php echo esc_attr($attributes->image_alt) ?>">
                 </div>
             </div>
-            <div class="basis-1/2 lg:pl-6 lg:mt-0 mt-14">
+            <div class="basis-1/2 tb:pl-6 tb:mt-0 mt-14">
                 <?php if (!empty($attributes->title) && strlen($attributes->title) > 0): ?>
                     <div>
                         <h3 class="h4"><?php echo esc_html($attributes->title); ?></h3>
