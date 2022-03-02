dbbd17986946e67ee68955994613bb1c2e4962dc
Braeden Craig <braedencraig@gmail.com>
2022-03-02 14:29:18 -0500

    ba-124 increase margin bottom

--- web/wp-content/themes/borealis-2021/inc/partials/hero/hero-header.php
+++ web/wp-content/themes/borealis-2021/inc/partials/hero/hero-header.php
@@ -7,7 +7,7 @@ if (!function_exists('pg_generate_hero_header')) {
 ?>
         <div>
             <canvas class="absolute top-0 canvas" id="canvas"></canvas>
-            <div class="w-full md:w-6/12 h1 relative z-10 pt-72 mb-36 text-shade-white-400"><?php echo esc_html(the_title()) ?></div>
+            <div class="w-full md:w-6/12 h1 relative z-10 pt-72 mb-72 text-shade-white-400"><?php echo esc_html(the_title()) ?></div>
         </div>
 <?php
         return ob_get_clean();
