967851bdaaebd9faf2bd1e2538c81665922cfed6
Braedencraig <braeden.craig@gmail.com>
2022-03-30 15:07:14 -0400

    BA-208 add margin to product page text-2-up so its closer to tabbable content

--- web/wp-content/themes/borealis-2021/inc/gutenberg/render-text-2-up-container-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/render-text-2-up-container-block.php
@@ -51,7 +51,7 @@ if (!function_exists('pg_render_text_2_up_container_block')) {
         $image = wp_get_attachment_image_url($attributes->image_id, 'full');
         ob_start();
 ?>
-        <div class="<?php echo $attributes->bgColour ?> custom-component animated-element">
+        <div class="<?php echo $attributes->bgColour ?> custom-component animated-element <?php echo is_singular('product') ? '-mb-10' : '' ?>">
             <div class="container flex <?php echo !empty($attributes->cta_text) ? 'md:pt-20 pt-10' : 'md:py-20 py-10' ?> tb:flex-row flex-col">
                 <div class="w-full tb:w-4/12">
                     <?php if ($attributes->text_or_image === 'image') : ?>
