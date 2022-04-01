2a96cdb6d8331df168aa1f2579b55c4f7883fc27
Braedencraig <braeden.craig@gmail.com>
2022-04-01 16:39:59 -0400

    BA-243 add in borderjump fix for search page

--- web/wp-content/themes/borealis-2021/dist/mainSearch.js
+++ web/wp-content/themes/borealis-2021/dist/mainSearch.js
@@ -1,49 +1,43 @@
-/******/
-(function() {
-    // webpackBootstrap
-    var __webpack_exports__ = {};
-    /*!***************************************!*\
-    !*** ./src/js/scripts/main-search.js ***!
-    \***************************************/
-    function mainSearch() {
-        const searchInput = document.getElementById("search");
-        const searchIconOpen = document.querySelector("#open-search");
-        const searchIconClose = document.querySelector("#close-search");
-        searchInput &&
-            searchInput.addEventListener("keyup", (e) => {
-                if (e.keyCode === 9) {
-                    searchInput.classList.add("focus:outline-4");
-                    searchInput.classList.remove("focus:outline-none");
-                }
-            });
-        searchInput &&
-            searchInput.addEventListener("focus", (e) => {
-                searchInput.classList.add("focus:outline-none");
-                searchInput.classList.remove("focus:outline-4");
-            });
-        searchIconClose &&
-            searchIconClose.addEventListener("click", () => {
-                if (!searchIconClose.classList.contains("hidden")) {
-                    searchInput.value = "";
-                    searchIconClose.classList.add("hidden");
-                    searchIconOpen.classList.remove("hidden");
-                }
-            });
-        searchInput &&
-            searchInput.addEventListener("input", (e) => {
-                if (e.target.value) {
-                    searchInput.classList.remove("text-shade-grey-700");
-                    searchInput.classList.add("text-primary-navy-400");
-                    searchIconClose && searchIconClose.classList.remove("hidden");
-                    searchIconOpen && searchIconOpen.classList.add("hidden");
-                } else {
-                    searchIconClose && searchIconClose.classList.add("hidden");
-                    searchIconOpen && searchIconOpen.classList.remove("hidden");
-                }
-            });
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***************************************!*\
+  !*** ./src/js/scripts/main-search.js ***!
+  \***************************************/
+function mainSearch() {
+  const searchInput = document.getElementById("search");
+  const searchIconOpen = document.querySelector("#open-search");
+  const searchIconClose = document.querySelector("#close-search");
+  searchInput && searchInput.addEventListener("keyup", e => {
+    if (e.keyCode === 9) {
+      searchInput.classList.add("focus:outline-4");
+      searchInput.classList.remove("focus:outline-none");
     }
+  });
+  searchInput && searchInput.addEventListener("focus", e => {
+    searchInput.classList.add("focus:outline-none");
+    searchInput.classList.remove("focus:outline-4");
+  });
+  searchIconClose && searchIconClose.addEventListener("click", () => {
+    if (!searchIconClose.classList.contains("hidden")) {
+      searchInput.value = "";
+      searchIconClose.classList.add("hidden");
+      searchIconOpen.classList.remove("hidden");
+    }
+  });
+  searchInput && searchInput.addEventListener("input", e => {
+    if (e.target.value) {
+      searchInput.classList.remove("text-shade-grey-700");
+      searchInput.classList.add("text-primary-navy-400");
+      searchIconClose && searchIconClose.classList.remove("hidden");
+      searchIconOpen && searchIconOpen.classList.add("hidden");
+    } else {
+      searchIconClose && searchIconClose.classList.add("hidden");
+      searchIconOpen && searchIconOpen.classList.remove("hidden");
+    }
+  });
+}
 
-    mainSearch();
-    /******/
-})();
+mainSearch();
+/******/ })()
+;
 //# sourceMappingURL=mainSearch.js.map
