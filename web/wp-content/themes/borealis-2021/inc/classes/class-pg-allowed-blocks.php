5d92b03f393c235dde320219d282f3fe547095db
Braedencraig <braeden.craig@gmail.com>
2022-04-28 16:42:49 -0400

    BA-412 video embed add

--- web/wp-content/themes/borealis-2021/inc/classes/class-pg-allowed-blocks.php
+++ web/wp-content/themes/borealis-2021/inc/classes/class-pg-allowed-blocks.php
@@ -82,7 +82,13 @@ class PG_Allowed_Blocks
 
             // Video Tabbed Content
             'video-tabbed-content-container-block',
-            'video-tabbed-content-panel-block'
+            'video-tabbed-content-panel-block',
+
+            // Video
+            'core/embed-youtube',
+            'core/video',
+            'core/embed'
+
         );
         $this->custom_blocks = array(
             // Blockquote
@@ -157,15 +163,24 @@ class PG_Allowed_Blocks
 
             // Spacer
             'spacer-container',
-            'spacer'
+            'spacer',
+
+            // Video
+            'core/embed-youtube',
+            'core/video',
+            'core/embed'
         );
         $this->shared_core_blocks = array(
             'core/embed-youtube',
             'core/video',
+            'core/embed'
         );
         $this->blog_core_blocks = array(
             'core/table',
             'core/quote',
+            'core/embed-youtube',
+            'core/video',
+            'core/embed'
         );
         $this->author_blocks = array(
             $this->namespace . '/author-meta-block',
--- web/wp-content/themes/borealis-2021/inc/gutenberg/render-video-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/render-video-block.php
@@ -32,7 +32,7 @@ if (!function_exists('pg_render_video_block')) {
         $image = wp_get_attachment_image_url($attributes->image_id, 'full');
         ob_start();
 ?>
-        <div class="custom-component nestable animated-element">
+        <!-- <div class="custom-component nestable animated-element">
             <div class="<?php echo !is_singular(array('news', 'research-blogs')) ? esc_attr('container') : '' ?> relative video-block rounded-large overflow-hidden pt-video md:pt-video-md lg:pt-video-lg tb:min-h-[350px] lg:min-h-[435px]">
                 <?php if (!empty($image)) : ?>
                     <div class="bg-cover bg-center absolute inset-0 z-10 video-block__overlay transition-all duration-400" style="background-image: url(<?php echo esc_url_raw($image) ?>)">
@@ -55,6 +55,34 @@ if (!function_exists('pg_render_video_block')) {
                 <?php if (!empty($attributes->caption)) : ?>
                     <p><?php echo esc_html($attributes->caption); ?></p>
                 <?php endif; ?>
+            </div>
+        </div> -->
+        <div id="<?php echo $attributes->anchor_id ?>" class="custom-component nestable animated-element component-padding video-tabbable custom-video">
+            <div class="flex w-full justify-end <?php echo is_singular(['research-blogs', 'news']) ? '' : 'container' ?>">
+                <div class="w-full tb:<?php echo is_singular(['research-blogs', 'news']) ? 'w-full' : 'w-8/12' ?> custom-video-panel">
+                    <div class="md:min-h-[350px] tb:min-h-[400px] lg:min-h-[540px] relative video-block rounded-large overflow-hidden pt-video md:pt-video-md lg:pt-video-lg">
+                        <?php if (!empty($attributes->image_url)) : ?>
+                            <div class="bg-cover bg-center absolute inset-0 z-10 video-block__overlay transition-all duration-400" style="background-image: url(<?php echo esc_url_raw($attributes->image_url) ?>)">
+                                <a class="block w-full h-full video-block__overlay__button" href="#" aria-label="<?php echo esc_attr('Play video') ?>">
+                                    <div class="flex w-full h-full items-center justify-center">
+                                        <span class="text-shade-white-400 paragraph-lg md:h2-desktop">
+                                            <?php
+                                            $icon = pg_render_icon('video-play');
+                                            echo wp_kses($icon, $allowed_html);
+                                            ?>
+                                        </span>
+                                        <p class="sr-only"><?php echo esc_html('Play video') ?></p>
+                                    </div>
+                                </a>
+                            </div>
+                        <?php endif; ?>
+                        <div class="flex w-full h-full justify-start tb:justify-center items-center absolute inset-0 bg-shade-black-400">
+                            <?php echo $block['innerContent'][0] ?>
+                            <!-- <video class="h-full w-full max-h-full" tabindex="-1" class="video" controls playsinline poster="<?php echo esc_url_raw($attributes->image_url) ?>" src="<?php echo esc_url_raw($attributes->video_url); ?>" type="video/mp4">
+                            </video> -->
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 <?php
