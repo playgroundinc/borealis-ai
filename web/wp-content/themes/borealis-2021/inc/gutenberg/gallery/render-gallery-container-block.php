9cefb092d30d455f7a06bf950353b82a66875b3d
Lisa Folkerson <lisa@playgroundinc.com>
2022-05-02 17:38:20 -0400

    BA-427 additional carousel sizing

--- web/wp-content/themes/borealis-2021/inc/gutenberg/gallery/render-gallery-container-block.php
+++ web/wp-content/themes/borealis-2021/inc/gutenberg/gallery/render-gallery-container-block.php
@@ -73,8 +73,8 @@ if (!function_exists('pg_render_gallery_container_block')) {
                 </div>
             </div>
             <div>
-                <div class="mx-auto md:ml-auto md:mr-0 w-full md:w-11/12 relative pt-gallery transition-all duration-400">
-                    <ul class="flex absolute left-0 top-0 bottom-0 max-w-full visible-scroll scrollbar-height overflow-x-scroll pl-6 items-center pr-6 md:pr-25 lg:pr-gallery-offset custom-gallery">
+                <div class="mx-auto w-full relative pt-gallery transition-all duration-400">
+                    <ul class="flex absolute left-0 top-0 bottom-0 max-w-full visible-scroll scrollbar-height overflow-x-scroll pl-2.9vw lg:pl-gallery-offset items-center pr-6 md:pr-25 lg:pr-gallery-offset custom-gallery">
                         <?php foreach ($block['innerBlocks'] as $index => $inner_block) : ?>
                             <?php if ($inner_block['blockName'] === $namespace . '/custom-image') : ?>
                                 <?php
@@ -86,7 +86,7 @@ if (!function_exists('pg_render_gallery_container_block')) {
                                 $image = wp_get_attachment_image_url($attributes->image_id, 'full');
                                 $placholder = wp_get_attachment_image_url($attributes->image_id);
                                 ?>
-                                <li class="nth-child-2:pl-5 pr-2.9vw">
+                                <li class="nth-child-2:pl-2vw pr-2.9vw">
 
                                     <picture>
                                         <img class="max-w-[64vw] w-[64vw] md:max-w-[30vw] md:w-[30vw] lg:max-w-[36vw] lg:w-36vw xl:max-w-[24vw] xl:w-24vw rounded-large overflow-hidden block" src="<?php echo esc_url_raw($image) ?>" alt="<?php echo !empty($attributes->image_alt) ? esc_attr($attributes->image_alt) : null ?>">
