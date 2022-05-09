dbe53347a530c8be842d20cc8a6640069b00fc4b
Braedencraig <braeden.craig@gmail.com>
2022-05-09 13:50:16 -0400

    BA-466 header height dynamic

---
+++ web/wp-content/themes/borealis-2021/dist/headerHeight.js
@@ -0,0 +1,25 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./src/js/scripts/headerHeight.js ***!
+  \****************************************/
+function headerHeight() {
+  const headerHeight = document.getElementsByClassName("nav-height-resize")[0];
+
+  if (headerHeight) {
+    const totalNavHeight = document.getElementById("main-navigation").offsetHeight + document.getElementsByClassName("current_page_item")[0].children[1].offsetHeight;
+    headerHeight.style.height = `${totalNavHeight + 20}px`;
+
+    const resizeHeader = () => {
+      const totalNavHeightResize = document.getElementById("main-navigation").offsetHeight + document.getElementsByClassName("current_page_item")[0].children[1].offsetHeight;
+      headerHeight.style.height = `${totalNavHeightResize + 20}px`;
+    };
+
+    window.addEventListener("resize", resizeHeader);
+  }
+}
+
+headerHeight();
+/******/ })()
+;
+//# sourceMappingURL=headerHeight.js.map
